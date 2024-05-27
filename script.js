6a172279c3d2cd256ddc944b04ab6a53ccea8202
happystonez <zhoustone233@gmail.com>
2024-05-27 15:45:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,10 @@ chapters.forEach(chapter => {
 // Add markers for each ocean region
 oceanRegions.forEach(region => {
     const marker = L.marker(region.coords).addTo(map);
-    marker.bindPopup(`<b>${region.name}</b><br>${region.description}<br><br><i>${region.intro}</i>`);
+    marker.bindPopup(`<b>${region.name}</b><br>
+    <img src="images/oceanRegions/${region.image}" alt="${region.name}" style="width:100%;height:auto;"><br>
+    ${region.description}<br><br>
+    <i>${region.intro}</i>`);
 });
 
 // Toggle chapter details in the sidebar
