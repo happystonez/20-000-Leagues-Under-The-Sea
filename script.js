6f953d4293f20d833b5884dda48fa166688a2902
happystonez <zhoustone233@gmail.com>
2024-06-07 16:45:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,26 +6,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add markers for each ocean region
-oceanRegions.forEach(region => {
-    const marker = L.marker(region.coords).addTo(map);
-    marker.bindPopup(`
-        <b>${region.name}</b><br>
-        <img src="oceanRegions/${region.image}" alt="${region.name}" style="width:100%;height:auto;"><br>
-        ${region.description}<br><br>
-        <i>${region.intro}</i>
-    `);
-});
-
 // Add markers for each chapter
 chapters.forEach(chapter => {
     const marker = L.marker(chapter.coords).addTo(map);
-    marker.bindPopup(`<b>${chapter.number}. ${chapter.title}</b><br>${chapter.plot}`);
+    marker.bindPopup(`<b>${chapter.title}</b><br>${chapter.plot}`);
     
     // Add chapter to list in the sidebar
     const li = document.createElement('li');
-    li.innerHTML = `<h3>${chapter.number}. ${chapter.title}</h3>`;
-    li.onclick = () => toggleChapterDetail(li);
+    li.textContent = `${chapter.number}. ${chapter.title}`;
+    li.onclick = () => toggleChapterDetail(chapter, li);
     const detail = document.createElement('div');
     detail.className = 'chapter-detail';
     detail.innerHTML = `
@@ -36,8 +25,14 @@ chapters.forEach(chapter => {
     document.getElementById('chapterList').appendChild(li);
 });
 
+// Add markers for each ocean region
+oceanRegions.forEach(region => {
+    const marker = L.marker(region.coords).addTo(map);
+    marker.bindPopup(`<b>${region.name}</b><br>${region.description}<br><br><i>${region.intro}</i>`);
+});
+
 // Toggle chapter details in the sidebar
-function toggleChapterDetail(li) {
+function toggleChapterDetail(chapter, li) {
     const detail = li.querySelector('.chapter-detail');
     detail.classList.toggle('show');
 }
